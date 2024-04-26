2ac7d7c9f96c8ac30acd2f2823e275aac8108ea0
Ellie <egs55@york.ac.uk>
2024-04-26 18:08:24 +0100

    update api + 1 graph

--- src/api/dmas.ts
+++ src/api/dmas.ts
@@ -1,9 +1,16 @@
 // given a range of cooridinates,
 // DMAS api will return all data in location range
 
+interface PlantGrowthDatum {
+    date: Date;
+    longitude: number;
+    latitiude: number;
+    count: number;
+}
+
 export interface DmasData {
     species: string;
-    quantityOnDates: Array<[Date, number]>;
+    plantGrowth: PlantGrowthDatum[];
 }
 
 export interface DmasAPI {
@@ -15,20 +22,242 @@ export const MockDmasAPI: DmasAPI = {
         return [
             {
                 species: "Rose",
-                quantityOnDates: [[new Date(2022, 1, 10),50],[new Date(2023, 1, 10),40],[new Date(2024, 1, 10),20]]
-            },
-            {
-                species: "Janpanese Knotweed",
-                quantityOnDates: [[new Date(2022, 1, 10),0],[new Date(2023, 1, 10),10],[new Date(2024, 1, 10),60]]
-            },
-            {
-                species: "Sweet Chestnut",
-                quantityOnDates: [[new Date(2022, 1, 10),10],[new Date(2023, 1, 10),12],[new Date(2024, 1, 10),20]]
-            },
-            {
-                species: "Bracken",
-                quantityOnDates: [[new Date(2022, 1, 10),20],[new Date(2023, 1, 10),20],[new Date(2024, 1, 10),15]]
-            },
-        ];
-    },
-};
+                plantGrowth: [{
+                    date: new Date(2022, 1, 10),
+                    longitude: -0.06664746,
+                    latitiude: -0.06236538,
+                    count: 10,
+                },{
+                    date: new Date(2022, 1, 10),
+                    longitude: -0.04116847,
+                    latitiude: -0.06236538,
+                    count: 15,
+                },{
+                    date: new Date(2022, 1, 10),
+                    longitude: -0.05116847,
+                    latitiude: -0.06236538,
+                    count: 25,
+                },
+                {
+                    date: new Date(2023, 1, 10),
+                    longitude: -0.06664746,
+                    latitiude: -0.06236538,
+                    count: 10,
+                },{
+                    date: new Date(2023, 1, 10),
+                    longitude: -0.04116847,
+                    latitiude: -0.06236538,
+                    count: 10,
+                },{
+                    date: new Date(2023, 1, 10),
+                    longitude: -0.05116847,
+                    latitiude: -0.06236538,
+                    count: 20,
+                },
+                {
+                    date: new Date(2024, 1, 10),
+                    longitude: -0.06664746,
+                    latitiude: -0.06236538,
+                    count: 6,
+                }, {
+                    date: new Date(2024, 1, 10),
+                    longitude: -0.04116847,
+                    latitiude: -0.06236538,
+                    count: 6,
+                }, {
+                    date: new Date(2024, 1, 10),
+                    longitude: -0.05116847,
+                    latitiude: -0.06236538,
+                    count: 12,
+                }
+            ]},{
+        
+                    species: "Japanese Knotweed",
+                    plantGrowth: [{
+                        date: new Date(2022, 1, 10),
+                        longitude: -0.06664746,
+                        latitiude: -0.06236538,
+                        count: 1,
+                    },{
+                        date: new Date(2022, 1, 10),
+                        longitude: -0.04116847,
+                        latitiude: -0.06236538,
+                        count: 5,
+                    },{
+                        date: new Date(2022, 1, 10),
+                        longitude: -0.05116847,
+                        latitiude: -0.06236538,
+                        count: 3,
+                    },
+                    {
+                        date: new Date(2023, 1, 10),
+                        longitude: -0.06664746,
+                        latitiude: -0.06236538,
+                        count: 10,
+                    },{
+                        date: new Date(2023, 1, 10),
+                        longitude: -0.04116847,
+                        latitiude: -0.06236538,
+                        count: 12,
+                    },{
+                        date: new Date(2023, 1, 10),
+                        longitude: -0.05116847,
+                        latitiude: -0.06236538,
+                        count: 15,
+                    },
+                    {
+                        date: new Date(2024, 1, 10),
+                        longitude: -0.06664746,
+                        latitiude: -0.06236538,
+                        count: 21,
+                    }, {
+                        date: new Date(2024, 1, 10),
+                        longitude: -0.04116847,
+                        latitiude: -0.06236538,
+                        count: 24,
+                    }, {
+                        date: new Date(2024, 1, 10),
+                        longitude: -0.05116847,
+                        latitiude: -0.06236538,
+                        count: 30,
+                    }]},{
+                    species: "Sweet Chestnut",
+                    plantGrowth: [{
+                        date: new Date(2022, 1, 10),
+                        longitude: -0.06664746,
+                        latitiude: -0.06236538,
+                        count: 2,
+                    },{
+                        date: new Date(2022, 1, 10),
+                        longitude: -0.04116847,
+                        latitiude: -0.06236538,
+                        count: 2,
+                    },{
+                        date: new Date(2022, 1, 10),
+                        longitude: -0.05116847,
+                        latitiude: -0.06236538,
+                        count: 3,
+                    },
+                    {
+                        date: new Date(2023, 1, 10),
+                        longitude: -0.06664746,
+                        latitiude: -0.06236538,
+                        count: 5,
+                    },{
+                        date: new Date(2023, 1, 10),
+                        longitude: -0.04116847,
+                        latitiude: -0.06236538,
+                        count: 4,
+                    },{
+                        date: new Date(2023, 1, 10),
+                        longitude: -0.05116847,
+                        latitiude: -0.06236538,
+                        count: 1,
+                    },
+                    {
+                        date: new Date(2024, 1, 10),
+                        longitude: -0.06664746,
+                        latitiude: -0.06236538,
+                        count: 10,
+                    }, {
+                        date: new Date(2024, 1, 10),
+                        longitude: -0.04116847,
+                        latitiude: -0.06236538,
+                        count: 4,
+                    }, {
+                        date: new Date(2024, 1, 10),
+                        longitude: -0.05116847,
+                        latitiude: -0.06236538,
+                        count: 5,
+                    }]},{
+                    species: "Bracken",
+                    plantGrowth: [{
+                        date: new Date(2022, 1, 10),
+                        longitude: -0.06664746,
+                        latitiude: -0.06236538,
+                        count: 6,
+                    },{
+                        date: new Date(2022, 1, 10),
+                        longitude: -0.04116847,
+                        latitiude: -0.06236538,
+                        count: 5,
+                    },{
+                        date: new Date(2022, 1, 10),
+                        longitude: -0.05116847,
+                        latitiude: -0.06236538,
+                        count: 5,
+                    },
+                    {
+                        date: new Date(2023, 1, 10),
+                        longitude: -0.06664746,
+                        latitiude: -0.06236538,
+                        count: 6,
+                    },{
+                        date: new Date(2023, 1, 10),
+                        longitude: -0.04116847,
+                        latitiude: -0.06236538,
+                        count: 5,
+                    },{
+                        date: new Date(2023, 1, 10),
+                        longitude: -0.05116847,
+                        latitiude: -0.06236538,
+                        count: 5,
+                    },
+                    {
+                        date: new Date(2024, 1, 10),
+                        longitude: -0.06664746,
+                        latitiude: -0.06236538,
+                        count: 6,
+                    }, {
+                        date: new Date(2024, 1, 10),
+                        longitude: -0.04116847,
+                        latitiude: -0.06236538,
+                        count: 5,
+                    }, {
+                        date: new Date(2024, 1, 10),
+                        longitude: -0.05116847,
+                        latitiude: -0.06236538,
+                        count: 8,
+                    }]}
+
+                
+            ]
+        }};
+                
+               
+            
+            
+            
+           
+
+// export interface DmasData {
+//     species: string;
+//     quantityOnDates: Array<[Date, number]>;
+// }
+
+// export interface DmasAPI {
+//     getDmasData(): Promise<DmasData[]>;
+// }
+
+// export const MockDmasAPI: DmasAPI = {
+//     getDmasData: async () => {
+//         return [
+//             {
+//                 species: "Rose",
+//                 quantityOnDates: [[new Date(2022, 1, 10),50],[new Date(2023, 1, 10),40],[new Date(2024, 1, 10),20]]
+//             },
+//             {
+//                 species: "Janpanese Knotweed",
+//                 quantityOnDates: [[new Date(2022, 1, 10),0],[new Date(2023, 1, 10),10],[new Date(2024, 1, 10),60]]
+//             },
+//             {
+//                 species: "Sweet Chestnut",
+//                 quantityOnDates: [[new Date(2022, 1, 10),10],[new Date(2023, 1, 10),12],[new Date(2024, 1, 10),20]]
+//             },
+//             {
+//                 species: "Bracken",
+//                 quantityOnDates: [[new Date(2022, 1, 10),20],[new Date(2023, 1, 10),20],[new Date(2024, 1, 10),15]]
+//             },
+//         ];
+//     },
+// };
--- src/pages/data/index.tsx
+++ src/pages/data/index.tsx
@@ -1,12 +1,60 @@
 import { SetStateAction, useState } from "react";
-import { DmasAPI, MockDmasAPI } from "../../api/dmas";
-import ReactDOM from "react-dom";
+import { DmasAPI, DmasData, MockDmasAPI } from "../../api/dmas";
+import { useQuery } from "@tanstack/react-query";
+import { Label, BarChart,LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Bar, Rectangle } from 'recharts';
+import { forEachChild } from "typescript";
 
 
 // TODO replace this with a real implementation
 const Plants: DmasAPI = MockDmasAPI;
-function formatDataBarChart(data: any){
-    console.log(Plants.getDmasData());
+
+function formatData(){
+    const dataArray = useQuery({
+        queryFn: Plants.getDmasData,
+        queryKey: ["species"],
+    });
+    const plantTotalsByYear = new Map<number, Record<string, number>>();
+    if (dataArray.data != undefined){
+        for(const x of dataArray.data){
+            for(const y of x.plantGrowth) {
+                const year = y.date.getFullYear();
+                if (!plantTotalsByYear.has(year)) {
+                    plantTotalsByYear.set(year, {});
+                }
+               plantTotalsByYear.get(year)![x.species] = 0;
+               plantTotalsByYear.get(year)![x.species] += y.count; 
+            } 
+        }
+    }
+    const plantTotalsArray = Array.from(plantTotalsByYear.entries())
+        .map(([year, totals]) => ({ year, ...totals }));
+    return plantTotalsArray   
+    }
+
+function returnAllSpecies() {
+    const allSpecies = [];
+    const returnData = useQuery({
+        queryFn: Plants.getDmasData,
+        queryKey: ["species"],
+      })
+      if(returnData.data != undefined){
+        for (const x of returnData.data) {
+            allSpecies.push(x.species)
+          }
+      }
+      return allSpecies;
+}
+function stringToColour(str: string){
+    let hash = 0;
+    str.split('').forEach(char => {
+      hash = char.charCodeAt(0) + ((hash << 5) - hash)
+    })
+    let colour = '#'
+    for (let i = 0; i < 3; i++) {
+      const value = (hash >> (i * 8)) & 0xff
+      colour += value.toString(16).padStart(2, '0')
+    }
+    return colour
 }
 //add year to form
 export default function DataPage(this: any) {
@@ -15,8 +63,10 @@ export default function DataPage(this: any) {
   const handleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
     setMyLocation(event.target.value)
   }
-  console.log(myLocation)
-  formatDataBarChart(Plants);
+  
+  const chartData = formatData();
+  const allSpecies = returnAllSpecies();
+  
     return (
         <div>
             <h1 className="text-2xl font-bold">View Data</h1>
@@ -27,6 +77,32 @@ export default function DataPage(this: any) {
                 <option value="London">London</option>
             </select>
             </form>
+            <ResponsiveContainer width={"50%"} height={500} >
+                <LineChart 
+                data={chartData}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="year" >
+                <Label value="Time" offset={-5} position="insideBottom" />
+                </XAxis>
+                <YAxis label={{ value: 'Plant count', angle: -90, position: 'insideLeft' }}>   
+                </YAxis>
+                <Tooltip />
+                <Legend />
+                
+                {
+                allSpecies.map((x) => (
+                    <Line type="monotone" dataKey={x} stroke={stringToColour(x)} />
+                ))}
+                
+                </LineChart>
+            </ResponsiveContainer>
         </div>
     )
     
